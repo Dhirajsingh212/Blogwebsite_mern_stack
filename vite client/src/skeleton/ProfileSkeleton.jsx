2b30ecbbc46d28271fb998e37c6a9aa9bc22141e
Dhirajsingh212 <dhirajsingh0229@gmail.com>
2024-03-15 01:43:13 +0530

    updated profile page ui

--- vite client/src/skeleton/ProfileSkeleton.jsx
+++ vite client/src/skeleton/ProfileSkeleton.jsx
@@ -5,7 +5,7 @@ const ProfileSkeleton = () => {
   return (
     <div className="py-10 profile_div_main">
       <div className="profile_div flex flex-col items-center justify-center">
-        <Skeleton className="h-48 w-48" circle />
+        <div className="skeleton w-48 h-48 bg-[#2b323e]  rounded-full shrink-0"></div>
         <Skeleton className="h-12 w-72 md:w-96" />
         <Skeleton className="h-12 w-72 md:w-96" />
         <Skeleton className="h-12 w-72 md:w-96" />
